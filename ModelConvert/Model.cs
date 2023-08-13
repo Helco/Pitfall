5af94f92bb1c56a9c5bde5eb9342afcbc24724c1
Helco <hermann.noll@hotmail.com>
2023-08-13 18:38:50 +0200

    Fix model structure in template and ModelConvert

---
+++ ModelConvert/Model.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using System.Net.WebSockets;
+using static Pitfall.Utils;
+
+namespace Pitfall;
+
+internal static class Utils
+{
+    public static T[] ReadArray<T>(BinaryReader reader, int count, Func<BinaryReader, T> readElement) =>
+        Enumerable.Repeat(reader, count).Select(readElement).ToArray();
+
+    public static Vector4 ReadVector4(BinaryReader r) => new Vector4(r.ReadSingle(), r.ReadSingle(), r.ReadSingle(), r.ReadSingle());
+    public static Byte4 ReadByte4(BinaryReader r) => new Byte4(r.ReadByte(), r.ReadByte(), r.ReadByte(), r.ReadByte());
+    public static Vector2 ReadVector2(BinaryReader r) => new Vector2(r.ReadSingle(), r.ReadSingle());
+    public static Vector4 ReadUnknownVector4(BinaryReader r) => ReadByte4(r).AsNormalized;
+    public static ushort ReadUShort(BinaryReader r) => r.ReadUInt16();
+}
+
+public interface IModelPart { }
+
+public readonly record struct Byte4(byte R, byte G, byte B, byte A)
+{
+    public Vector4 AsNormalized => new Vector4(R, G, B, A) / 255f;
+}
+
+[Flags]
+internal enum GeometryFlags : uint
+{
+    HasTexCoords = 0x02,
+    HasColors = 0x04,
+    HasNormals = 0x08,
+    IsHalfPrecision = 0x10,
+    HasIndices = 0x20,
+    DisableIndices = 0x1000_0000
+}
+
+internal enum PartType : byte
+{
+    Geometry,
+    SetByte,
+    EnableUnknown2,
+    DisableUnknown3,
+    EnableUnknown4,
+    DisableUnknown5,
+    End
+}
+
+public class GeometryPart : IModelPart
+{
+    public Vector4[] Positions { get; }
+    public Vector2[]? TexCoords { get; }
+    public Byte4[]? Colors { get; }
+    public Vector3[]? Normals { get; }
+    public Vector4[]? UnknownVector { get; }
+    public ushort[]? Indices { get; }
+
+    private static float GetNormalized(uint value, int offset, int bits)
+    {
+        uint maxValue = 1u << bits;
+        value = (value >> offset) & (maxValue - 1u);
+        if ((value & (maxValue >> 1)) > 0)
+            value -= maxValue;
+        return value / (float)maxValue;
+    }
+
+    private static Vector3 ReadNormal(BinaryReader r)
+    {
+        var value = r.ReadUInt32();
+        var x = GetNormalized(value, 0, 11);
+        var y = GetNormalized(value, 11, 11);
+        var z = GetNormalized(value, 22, 10);
+        return new Vector3(x, y, z);
+    }
+
+    internal GeometryPart(BinaryReader reader, GeometryFlags flags, bool hasUnknownVectors)
+    {
+        if (flags.HasFlag(GeometryFlags.IsHalfPrecision))
+            throw new NotSupportedException("Unsupported half-precision model");
+
+        int vertexCount = reader.ReadInt32();
+        Positions = ReadArray(reader, vertexCount, ReadVector4);
+        TexCoords = flags.HasFlag(GeometryFlags.HasTexCoords)
+            ? ReadArray(reader, vertexCount, ReadVector2)
+            : null;
+        Colors = flags.HasFlag(GeometryFlags.HasColors)
+            ? ReadArray(reader, vertexCount, ReadByte4)
+            : null;
+        Normals = flags.HasFlag(GeometryFlags.HasNormals)
+            ? ReadArray(reader, vertexCount, ReadNormal)
+            : null;
+        UnknownVector = hasUnknownVectors
+            ? ReadArray(reader, vertexCount, ReadUnknownVector4)
+            : null;
+        if (flags.HasFlag(GeometryFlags.HasIndices) && !flags.HasFlag(GeometryFlags.DisableIndices))
+        {
+            var indexCount = reader.ReadInt32();
+            Indices = ReadArray(reader, indexCount, ReadUShort);
+        }
+        else
+            Indices = null;
+    }
+
+    public ushort[] GenerateImplicitIndices()
+    {
+        var vertexCount = Positions.Length;
+        var indices = new ushort[6 * vertexCount];
+        for (int i = 0; i < vertexCount - 2; i += 2)
+        {
+            indices[i * 6 + 0] = (ushort)(i + 0);
+            indices[i * 6 + 1] = (ushort)((i + 2) % vertexCount);
+            indices[i * 6 + 2] = (ushort)(i + 1);
+            indices[i * 6 + 3] = (ushort)(i + 1);
+            indices[i * 6 + 4] = (ushort)((i + 2) % vertexCount);
+            indices[i * 6 + 5] = (ushort)((i + 3) % vertexCount);
+        }
+        return indices;
+    }
+}
+
+public class SetUnknownBytePart : IModelPart
+{
+    public ushort Index { get; }
+    public byte Value { get; }
+
+    public SetUnknownBytePart(BinaryReader reader)
+    {
+        Index = reader.ReadUInt16();
+        Value = reader.ReadByte();
+    }
+}
+
+public class SubSubModel
+{
+    public uint ID { get; }
+    public uint GeometryCount { get; } // as reported by the model
+    public IReadOnlyList<IModelPart> Parts { get; }
+
+    public SubSubModel(BinaryReader reader)
+    {
+        ID = reader.ReadUInt32();
+        var flags = (GeometryFlags)reader.ReadUInt32();
+        GeometryCount = reader.ReadUInt32();
+        var parts = new List<IModelPart>();
+        Parts = parts;
+        var hasUnknownVectors = false;
+
+        PartType type;
+        do
+        {
+            type = (PartType)reader.ReadByte();
+            switch (type)
+            {
+                case PartType.Geometry: parts.Add(new GeometryPart(reader, flags, hasUnknownVectors)); break;
+                case PartType.SetByte: parts.Add(new SetUnknownBytePart(reader)); break;
+                case PartType.EnableUnknown2:
+                case PartType.EnableUnknown4: hasUnknownVectors = true; break;
+                case PartType.DisableUnknown3:
+                case PartType.DisableUnknown5: hasUnknownVectors = false; break;
+                case PartType.End: break;
+                default: throw new NotSupportedException($"Unsupported part type: {type}");
+            }
+        } while (type != PartType.End);
+    }
+}
+
+public class SubModel
+{
+    public int Unknown { get; }
+    public IReadOnlyList<SubSubModel> SubSubModels { get; }
+
+    public SubModel(BinaryReader reader)
+    {
+        Unknown = reader.ReadInt32();
+        var count = reader.ReadInt32();
+        SubSubModels = ReadArray(reader, count, r => new SubSubModel(r));
+    }
+}
+
+public class Model
+{
+    public string Name { get; }
+    public float UnknownFloat { get; }
+    public IReadOnlyList<SubModel> SubModels { get; }
+
+    public Model(BinaryReader reader)
+    {
+        var zeroBytes = reader.ReadBytes(6);
+        if (zeroBytes.Any(b => b != 0))
+            throw new InvalidDataException("Expected first six bytes to be zero");
+
+        var nameBytes = new List<byte>();
+        while(true)
+        {
+            var b = reader.ReadByte();
+            if (b == 0)
+                break;
+            nameBytes.Add(b);
+        }
+        Name = System.Text.Encoding.UTF8.GetString(nameBytes.ToArray());
+
+        if (reader.ReadByte() != 0)
+            throw new InvalidDataException("Expected another zero byte");
+        UnknownFloat = reader.ReadSingle();
+
+        var subModelCount = reader.ReadInt32();
+        SubModels = ReadArray(reader, subModelCount, r => new SubModel(r));
+    }
+}
--- ModelConvert/ModelConvert.cs
+++ ModelConvert/ModelConvert.cs
@@ -1,13 +1,12 @@
-﻿namespace ModelConvert;
-using System;
+﻿using System;
 using System.IO;
 using System.Reflection.PortableExecutable;
 
+namespace Pitfall;
+
 internal class Program
 {
-    static void Main(string[] args) => MainFindVertexBlocks(args);
-
-    static void MainCheckBoneCounts(string[] args)
+    static void Main(string[] args)
     {
         var files = Directory.GetFiles(@"C:\Users\Helco\Downloads\PITFALL The Lost Expedition PC\PITFALL The Lost Expedition\Game\data\models");
         foreach (var file in files)
@@ -17,195 +16,8 @@ internal class Program
             var name = Path.GetFileName(file);
             using var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
             using var reader = new BinaryReader(fileStream);
-            var bytes = reader.ReadBytes(6);
-            while (reader.ReadByte() != 0) ;
-            reader.ReadByte(); // these two properties are
-            reader.ReadSingle(); // constant over all models
 
-            int subModelCount = reader.ReadInt32();
-            reader.ReadInt32();
-            int variantCount = reader.ReadInt32();
-            reader.ReadUInt32(); // id
-            reader.ReadUInt32(); // vertexFlags
-
-            if (subModelCount == 0 || variantCount == 0)
-                continue;
-
-            var boneCount = reader.ReadByte();
-            if (boneCount != 1)
-            Console.WriteLine($"{boneCount:d3} - {name}");
-        }
-    }
-
-    static void MainFindVertexBlocks(string[] args)
-    {
-        var bytes = File.ReadAllBytes(@"C:\Users\Helco\Downloads\PITFALL The Lost Expedition PC\PITFALL The Lost Expedition\Game\data\models\leechmodel");
-        var zeroWords = Enumerable
-            .Range(16, bytes.Length - 4 - 16)
-            .Where(i => bytes[i] < 4 && bytes[i + 1] == 0 && bytes[i + 2] == 0 && bytes[i + 3] == 0)
-            .Where(i => isGoodFloat(i - 12) && isGoodFloat(i - 8) && isGoodFloat(i - 4))
-            .Where(i => BitConverter.ToSingle(bytes, i - 12) != 0f || BitConverter.ToSingle(bytes, i - 8) != 0f || BitConverter.ToSingle(bytes, i - 4) != 0f)
-            .Select(i => i - 12)
-            .ToArray();
-
-        var blocks = new List<(int start, int size)>();
-        var curStart = zeroWords.First();
-        var curEnd = curStart;
-        foreach (var i in zeroWords.Skip(1))
-        {
-            if (i < curEnd + 16)
-                continue;
-            if (i == curEnd + 16)
-                curEnd = i;
-            else
-            {
-                while (Array.BinarySearch(zeroWords, curStart - 16) >= 0)
-                    curStart -= 16;
-                blocks.Add((curStart, curEnd - curStart + 16));
-                curStart = curEnd = i;
-            }
-        }
-        blocks.Add((curStart, curEnd - curStart + 16));
-
-        var plausibleBlocks = blocks.Where(b => b.size >= 2 * 16);
-        foreach (var block in plausibleBlocks)
-            Console.WriteLine($"{block.start:X8} - {block.size:X4} - {block.size / 16:X4} vertices");
-
-        var firstVariant = plausibleBlocks.Where(b => b.start < 0x17C47);
-        Console.WriteLine($"There are {firstVariant.Count()} blocks before 0x17C47");
-
-        foreach (var block in firstVariant)
-            Console.WriteLine($"{block.start:X8} -> " + NicerHexString(block.start - 16, 16));
-
-        bool isGoodFloat(int i)
-        {
-            float f = BitConverter.ToSingle(bytes, i);
-            return float.IsFinite(f) && MathF.Abs(f) < 1000f;
-        }
-
-        string NicerHexString(int start, int count) => string.Join(" ",
-            Enumerable.Range(0, count / 4).Select(i => Convert.ToHexString(bytes, start + i * 4, 4)));
-    }
-
-    static void MainMatch(string[] args)
-    {
-        var matchFinder = new MatchFinder();
-        matchFinder.Actual.Load("actual.csv");
-
-        string attr = "third-4b-attr";
-
-        matchFinder.Expected.LoadForProperty(attr + ".txt", attr);
-
-        for (uint i = 1; i <= 1; i++)
-        {
-            Console.WriteLine($"For {i}: " + string.Join(", ", matchFinder.FindActualPropertyForExpected(attr, i, filterZero: false)));
-            Console.WriteLine($"For {i} zero filtered: " + string.Join(", ", matchFinder.FindActualPropertyForExpected(attr, i, filterZero: true)));
-            Console.WriteLine();
-        }
-
-    }
-
-    static void MainScan(string[] args)
-    {
-        var lists = new string[32];
-        var nlists = new string[32];
-        var counts = new int[32];
-        /*void addByBit(string name, uint val, int i)
-        {
-            
-            if ((val & (1 << i)) > 0)
-            {
-                counts![i]++;
-                if (lists![i] == null)
-                    lists[i] = name;
-                else
-                    lists[i] += "\n" + name;
-            }
-            else
-            {
-                if (nlists![i] == null)
-                    nlists[i] = name;
-                else
-                    nlists[i] += "\n" + name;
-            }
-        }
-        void addByBits(string name, uint val)
-        {
-            for (int i = 0; i < 32; i++)
-            {
-                addByBit(name, val, i);
-            }
-        }*/
-
-        var t = 0;
-        //var min = 1000;
-        //var max = -10000;
-        var set = new Dictionary<int, int>();
-
-        var f = new Dictionary<string, uint>();
-        var files = Directory.GetFiles(@"C:\Users\Helco\Downloads\PITFALL The Lost Expedition PC\PITFALL The Lost Expedition\Game\data\models");
-
-        var propertySet = new PropertyValues();
-
-        foreach (var file in files)
-        {
-            if (file.Contains("files.list"))
-                continue;
-            var name = Path.GetFileName(file);
-            t++;
-            using var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
-            using var reader = new BinaryReader(fileStream);
-            var bytes = reader.ReadBytes(6);
-            while (reader.ReadByte() != 0) ;
-            reader.ReadByte(); // these two properties are
-            reader.ReadSingle(); // constant over all models
-
-
-            var count1 = reader.ReadUInt32();
-            var i2 = reader.ReadUInt32();
-            var count2 = reader.ReadUInt32();
-            uint id = reader.ReadUInt32();
-            var countOrFlags = reader.ReadUInt16();
-            var pageSize = reader.ReadUInt16();
-            var count4 = reader.ReadUInt32();
-            var bb = reader.ReadByte();
-            uint conditional1 = 0xCDCDCDCD;
-            byte conditional2 = 0xCD;
-            if (bb != 0 && bb != 4)
-                conditional1 = reader.ReadUInt32();
-            if (bb == 4)
-                conditional2 = reader.ReadByte();
-
-            propertySet.Add(name, nameof(count1), count1);
-            propertySet.Add(name, nameof(i2), i2);
-            propertySet.Add(name, nameof(count2), count2);
-            propertySet.Add(name, nameof(countOrFlags), countOrFlags);
-            propertySet.AddBits(name, nameof(countOrFlags), countOrFlags, 16);
-            propertySet.Add(name, nameof(pageSize), pageSize);
-            propertySet.AddBits(name, nameof(pageSize), pageSize, 16);
-            propertySet.Add(name, nameof(count4), count4);
-            propertySet.AddBits(name, nameof(count4), count4, 32);
-            propertySet.Add(name, nameof(bb), bb);
-            propertySet.AddBits(name, nameof(bb), bb, 8);
-            propertySet.Add(name, nameof(conditional1), conditional1);
-            propertySet.AddBits(name, nameof(conditional1), conditional1, 32);
-            propertySet.Add(name, nameof(conditional2), conditional2);
-            propertySet.AddBits(name, nameof(conditional2), conditional2, 8);
-        }
-
-        propertySet.Save("actual.csv");
-
-        for (int i = 0; i < 32; i++)
-        {
-            File.WriteAllText($"{i}.txt", lists[i]);
-            Console.WriteLine($"{i} {counts[i]} {counts[i] * 100 / t}");
-        }
-
-        foreach (var g in f.GroupBy(f => f.Value).OrderBy(g => g.Key))
-        {
-            Console.WriteLine(g.Key + " " + g.Count());
-            Console.WriteLine(String.Join(", ", g.Select(k => k.Key)));
-            Console.WriteLine();
+            new Model(reader);
         }
     }
 }
